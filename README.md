d9d7ee45c449bfc8ef1a18cf1bd92a7a58627a39
Priyanshu Bhattacharjee <pbo4oh@gmail.com>
2021-05-28 09:18:18 +0530

    Added Files

---
+++ README.md
@@ -0,0 +1,14 @@
+# Fake News Classifier
+
+#### This is an Fake News Classifier Project that can classify the news into fake news or real news
+
+## Developers
+
+#### Ayush Singh - AI/ML
+
+#### Priyanshu Bhattacharjee - Backend
+
+#### Ankush Chauhan - Frontend
+
+#### Darshan Mandade - Frontend
+
---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+import pickle
+
+app = Flask('/')
+
+# Routes
+@app.route('/', methods=['GET', 'POST'])
+def index():
+
+	# When method is POST take input and output it
+	if request.method == 'POST':
+		# Getting textual data from the input field
+		news = request.form['news_input']
+		res = news
+
+		# Rendering the output to the output box
+		return render_template('index.html', classified=res)
+
+	if request.method == 'GET':
+		return render_template('index.html')
+
+# For HotReload/Live Reload
+if __name__ == '__main__':
+	app.run(port='8080', debug=True)
